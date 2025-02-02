304a06abb58589e0102076c65f07b3a2374f9c62
Thomas V Varghese <118618681+www-domain@users.noreply.github.com>
2025-02-02 21:56:09 +0530

    Update environment.ts

--- micro-services/frontend/src/environments/environment.ts
+++ micro-services/frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.49.2.nip.io:80/api',
-    authServiceUrl: 'http://192.168.49.2.nip.io:80/api/auth',
-    paymentServiceUrl: 'http://192.168.49.2.nip.io:80/api/payments'
+    apiUrl: 'http://localhost:80/api',
+    authServiceUrl: 'http://localhost:80/api/auth',
+    paymentServiceUrl: 'http://localhost:80/api/payments'
   };
